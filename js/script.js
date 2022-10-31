5e5d36d4aac3028dc1e196881f7b6e1a296ba7b7
H3K83 <khk.warchild@gmail.com>
2022-10-30 22:58:34 -0500

    First commit with, like, everything

---
+++ js/script.js
@@ -0,0 +1,34 @@
+//Take and download screenshot with html2canvas
+function capture() {
+  const captureElement = document.querySelector("#capture");
+  html2canvas(captureElement, {
+    backgroundColor: null,
+  })
+    .then((canvas) => {
+      canvas.style.display = "none";
+      document.body.appendChild(canvas);
+      return canvas;
+    })
+    .then((canvas) => {
+      const image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+      const a = document.createElement("a");
+      a.setAttribute("download", "image.png");
+      a.setAttribute("href", image);
+      a.click();
+      canvas.remove();
+    });
+}
+
+const btn = document.querySelector("#capture-btn");
+btn.addEventListener("click", capture);
+
+//Convert rich content to plain text in editable divs
+let allEditableDivs = document.querySelectorAll('[contenteditable]');
+
+[].forEach.call(allEditableDivs, function (el) {
+  el.addEventListener('paste', function(e) {
+    e.preventDefault();
+    var text = e.clipboardData.getData("text/plain");
+    document.execCommand("insertHTML", false, text);
+  }, false);
+});
